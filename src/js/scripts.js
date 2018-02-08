ee36fbd314f1ef2900dd49e431e1298f3d6c4502
Pedro Muraki <pedromuraki@gmail.com>
2018-02-08 12:28:13 -0200

    Some JS errors solved

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,11 @@
 import { union } from 'ramda';
 
+import sum from './utils';
+
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6, 10, 15];
 const arr3 = union(arr1, arr2);
 
 console.log(arr3);
-
-import sum from './utils';
 
 console.log(sum(5, 2));
